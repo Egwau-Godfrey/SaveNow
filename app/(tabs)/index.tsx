b0b5797f7e4ff564444bbbbcadbd1422dbf5cfaa
Egwau-Godfrey <egwaujunior@gmail.com>
2025-06-21 18:51:50 +0300

    extra additions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 const currentDate = new Date();
 const year = currentDate.getFullYear();
@@ -20,48 +20,87 @@ export default function Home() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.titleHeader}>
-        <View>
-          <Text style={{fontWeight: "bold", fontSize: 18}}>Finance Overview</Text>
-          <Text>{day} {month} {year}</Text>
+        <View style={styles.titleHeader}>
+            <View>
+                <Text style={{fontWeight: "bold", fontSize: 18}}>Finance Overview</Text>
+                <Text>{day} {month} {year}</Text>
+            </View>
+            <View style={styles.userIconBackground}>
+                <Ionicons name="person" color={"white"} size={20}/>
+            </View>
         </View>
-        <View style={styles.userIconBackground}>
-          <Ionicons name="person" />
-        </View>
-      </View>
       
-      <View style={styles.bigCard}>
-        <View style={styles.cardFigures}>
-          <Text style={{color: "white", fontWeight: "500"}}>Total Balance</Text>
-          <Text style={{fontSize: 30, color: "white", fontWeight: "bold"}}>UGX {formattedBalance}</Text>
+        <View style={styles.bigCard}>
+            <View style={styles.cardFigures}>
+                <Text style={{color: "white", fontWeight: "500"}}>Total Balance</Text>
+                <Text style={{fontSize: 30, color: "white", fontWeight: "bold"}}>UGX {formattedBalance}</Text>
+            </View>
+            
+            <View style={styles.smallCardArea}>
+                <View style={styles.smallCard}>
+                    <View style={styles.smallCardIcon}>
+                        <Ionicons name="wallet" size={20} color="#a78bfa" />
+                    </View>
+                    <Text style={styles.smallCardLabel}>Income</Text>
+                    <Text allowFontScaling={false} style={styles.smallCardAmount}>UGX {formattedIncome}</Text>
+                </View>
+                
+                <View style={styles.smallCard}>
+                    <View style={styles.smallCardIcon}>
+                        <Ionicons name="gift" size={20} color="#a78bfa" />
+                    </View>
+                    <Text style={styles.smallCardLabel}>Bonuses</Text>
+                    <Text allowFontScaling={false} style={styles.smallCardAmount}>UGX {formattedBonuses}</Text>
+                </View>
+                
+                <View style={styles.smallCard}>
+                    <View style={styles.smallCardIcon}>
+                        <Ionicons name="cash" size={20} color="#a78bfa" />
+                    </View>
+                    <Text style={styles.smallCardLabel}>Others</Text>
+                    <Text allowFontScaling={false} style={styles.smallCardAmount}>UGX {formattedOthers}</Text>
+                </View>
+            </View>
         </View>
-        
-        <View style={styles.smallCardArea}>
-          <View style={styles.smallCard}>
-            <View style={styles.smallCardIcon}>
-              <Ionicons name="wallet" size={20} color="#a78bfa" />
+
+        <View style={styles.rowsArea}>
+            <View style={styles.firstRow}>
+                <Pressable style={styles.rowBtn}>
+                    <Ionicons name="add-circle-outline" size={42} color={"#a78bfa"} style={{fontWeight: "bold"}}/>
+
+                    <Text>
+                        Add Transaction
+                    </Text>
+                </Pressable>
+
+                <Pressable style={styles.rowBtn}>
+                    <Ionicons name="card-outline" size={42} color={"#a78bfa"} style={{fontWeight: "bold"}}/>
+
+                    <Text>
+                        View Expenses
+                    </Text>
+                </Pressable>
             </View>
-            <Text style={styles.smallCardLabel}>Income</Text>
-            <Text allowFontScaling={false} style={styles.smallCardAmount}>UGX {formattedIncome}</Text>
-          </View>
-          
-          <View style={styles.smallCard}>
-            <View style={styles.smallCardIcon}>
-              <Ionicons name="gift" size={20} color="#a78bfa" />
+
+            <View style={styles.secondRow}>
+                <Pressable style={styles.rowBtn}>
+                    <Ionicons name="trophy" size={42} color={"#a78bfa"} style={{fontWeight: "bold"}}/>
+
+                    <Text>
+                        Savings Goals
+                    </Text>
+                </Pressable>
+
+                <Pressable style={styles.rowBtn}>
+                    <Ionicons name="analytics" size={42} color={"#a78bfa"} style={{fontWeight: "bold"}}/>
+
+                    <Text>
+                        Report
+                    </Text>
+                </Pressable>
             </View>
-            <Text style={styles.smallCardLabel}>Bonuses</Text>
-            <Text allowFontScaling={false} style={styles.smallCardAmount}>UGX {formattedBonuses}</Text>
-          </View>
-          
-          <View style={styles.smallCard}>
-            <View style={styles.smallCardIcon}>
-              <Ionicons name="cash" size={20} color="#a78bfa" />
-            </View>
-            <Text style={styles.smallCardLabel}>Others</Text>
-            <Text allowFontScaling={false} style={styles.smallCardAmount}>UGX {formattedOthers}</Text>
-          </View>
         </View>
-      </View>
+
     </SafeAreaView>
   );
 }
@@ -128,5 +167,44 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#333",
     textAlign: "center",
+  },
+  rowsArea: {
+    width: "95%",
+    alignSelf: "center",
+    marginTop: 20,
+    paddingHorizontal: 5
+  },
+  firstRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 15
+  },
+  secondRow: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  rowBtn: {
+    height: 100,
+    width: "48%",
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 15,
+    alignItems: "center",
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  btnText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#333",
+    marginTop: 5
   }
 });
